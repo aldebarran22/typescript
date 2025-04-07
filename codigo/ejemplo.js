891b0190cfc99f80db6dfc32e3a9778f354d6449
aldebarran22 <antonioespinherranz@hotmail.com>
2025-04-07 11:18:18 +0200

    787

---
+++ codigo/ejemplo.js
@@ -0,0 +1,3 @@
+// Ejemplo para ejecutar con typescript
+var nombre = "TypeScript";
+console.log("Prueba con ".concat(nombre));
---
+++ codigo/ejemplo.ts
@@ -0,0 +1,3 @@
+// Ejemplo para ejecutar con typescript
+let nombre: string = "TypeScript"
+console.log(`Prueba con ${nombre}`)
